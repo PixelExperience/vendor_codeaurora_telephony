3519b8aba397dcc1ce313ca52c33f0341e002415
Dan Pasanen <dan.pasanen@gmail.com>
2016-12-29 09:08:07 -0600

    telephony: Allow devices to provide their own telephony-ext
    * Pixel devices specifically ship their own variants of telephony-ext
      and ims-common-ext.
    
    Change-Id: I610d28aa8baaaef4ae4eeb2dcdc115335e3d9b73

--- internal/Android.mk
+++ internal/Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(TARGET_PROVIDES_TELEPHONY_EXT),)
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -15,3 +17,5 @@ include $(BUILD_JAVA_LIBRARY)
 # Include subdirectory makefiles
 # ============================================================
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+endif
