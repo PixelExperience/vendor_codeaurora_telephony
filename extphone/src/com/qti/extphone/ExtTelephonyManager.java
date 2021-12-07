b275c52257b4d021f65e8203c2d50055cdc13c5c
Akshaj Anil Patil <quic_akshajan@quicinc.com>
2021-12-07 18:32:19 +0530

    Primary IMEI FR70532 changes
    - Add getImeiInfo() API over IExtPhone.aidl
    - Add onImeiTypeChanged() callback method over
      IExtPhoneCallback.aidl
    
    CRs-Fixed: 3049743
    Change-Id: Idb5ec984d60df56d3581b5cd5980a6e7c15b960c

--- extphone/src/com/qti/extphone/ExtTelephonyManager.java
+++ extphone/src/com/qti/extphone/ExtTelephonyManager.java
@@ -700,6 +700,22 @@ public class ExtTelephonyManager {
         }
     }
 
+   /**
+    * To get the IMEI information of all slots on device.
+    * @return
+    *        QtiImeiInfo[], contains array of imeiInfo(i.e slotId, IMEI string and IMEI type).
+    *
+    * The calling application should not assume returned array index as slotId, instead the
+    * application has to use the slotId that present in QtiImeiInfo object to know the IMEI
+    * corresponds to a slot.
+    *
+    * Requires Permission: android.Manifest.permission.READ_PRIVILEGED_PHONE_STATE
+    */
+    public QtiImeiInfo[] getImeiInfo() {
+        Log.e(LOG_TAG, "getImeiInfo not supported");
+        // TODO : Get & Return IMEI info from ExtTelephonyServiceImpl.java
+        return null;
+    }
 
     public boolean isSmartDdsSwitchFeatureAvailable() throws RemoteException {
         try {
